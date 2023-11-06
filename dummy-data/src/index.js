0498af30cd2518857ebef74721cee906e5787927
sandhyashivalikar <130589681+sandhyashivalikar@users.noreply.github.com>
2023-11-06 21:36:26 +0530

    Add files via upload

---
+++ dummy-data/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <input type="hidden" value="21BOE10049"/>
+    <input type="hidden" value"https://github.com/sandhyashivalikar - follow me on github" />
+  </React.StrictMode>
+);
